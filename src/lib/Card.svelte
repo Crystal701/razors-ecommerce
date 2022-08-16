b2830b590ebfa1b3192b6b5d12a8aa8174107e84
Crystal Koo <96crystal.k@gmail.com>
2022-08-16 17:12:00 +0800

    add await

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -14,21 +14,23 @@
     <h3 class="font-secondary text-2xl">{title}</h3>
 
     <div class="grid grid-cols-2 lg:grid-cols-4 gap-6 my-10 h-full">
-        {#each productCategory as { id, title, image, price } (id)}
-            <a
-                sveltekit:prefetch
-                href="/products/{id}"
-                class="flex flex-col shadow-md bg-[rgba(255,255,255,0.1)] p-6 h-stretch border-t-4 border-[#ba9761] hover:border-black hover:shadow-xl transition duration-300"
-            >
-                <img src={image.url} alt={title} class="my-4" />
-
-                <div class="flex flex-col justify-between h-full">
-                    <h4>{title}</h4>
-                    <p class="font-semibold">${price}</p>
-                </div>
-            </a>
-        {:else}
+        {#await productCategory}
             <Loading />
-        {/each}
+        {:then productCategory}
+            {#each productCategory as { id, title, image, price } (id)}
+                <a
+                    sveltekit:prefetch
+                    href="/products/{id}"
+                    class="flex flex-col shadow-md bg-[rgba(255,255,255,0.1)] p-6 h-stretch border-t-4 border-[#ba9761] hover:border-black hover:shadow-xl transition duration-300"
+                >
+                    <img src={image.url} alt={title} class="my-4" />
+
+                    <div class="flex flex-col justify-between h-full">
+                        <h4>{title}</h4>
+                        <p class="font-semibold">${price}</p>
+                    </div>
+                </a>
+            {/each}
+        {/await}
     </div>
 </div>
